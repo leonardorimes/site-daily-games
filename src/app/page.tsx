93b7e67aed29b4dbd8e1daf0c8d2d914ecd8af18
Leonardo Microlins <rimesleo@gmail.com>
2025-05-23 18:00:35 -0300

    criando a home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
+import { Container } from "@/components/header/container";
 import Image from "next/image";
+import { GameProps } from "@/utils/types/game";
+import Link from "next/link";
 
-export default function Home() {
+async function getDailyGame() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`
+    );
+
+    return res.json;
+  } catch (err) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
+export default async function Home() {
+  const dailyGame: GameProps = await getDailyGame();
+
+  console.log(dailyGame);
+
   return (
     <main className="flex">
-      <h1>Teste</h1>
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8">
+          Separamos um jogo exclusivo para você
+        </h1>
+        <Link href={`game/${dailyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <Image
+              src={dailyGame.image_url}
+              alt={dailyGame.title}
+              priority={true}
+              quality={100}
+              width={100}
+              height={100}
+            />
+          </section>
+        </Link>
+      </Container>
     </main>
   );
 }
