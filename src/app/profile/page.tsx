a52ef2b636ff0a138abad7e546d2effad3db4c5f
Leonardo Rimes <rimesleo@gmail.com>
2025-06-17 11:21:39 -0300

    criando pagina profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,34 @@
+import { Container } from "@/components/container";
+import Image from "next/image";
+import userImg from "public/user.png";
+import { FaShareAlt } from "react-icons/fa";
+
+export default function Profile() {
+  return (
+    <main className="w-full text-black">
+      <Container>
+        <section className="mt-8 mb-6 flex flex-col items-center justify-between relative gap-3 sm:flex-row">
+          <div className="w-full flex items-center gap-4 text-lg flex-col sm:flex-row justify-center sm:justify-normal">
+            <Image
+              src="/user.png" // Caminho direto a partir da raiz de public
+              alt="Imagem perfil do usuario"
+              width={224} // necessário com `next/image` se não usar layout="fill"
+              height={224}
+              className="rounded-full w-56 h-56 object-cover"
+            />
+
+            <h1 className="font-bold text-2xl">Leonardo Rimes</h1>
+          </div>
+          <div className="sm:absolute top-0 right-0 gap-3 flex items-center justify-center">
+            <button className="bg-gray-700 px-4 py-3 rounded-lg text-white">
+              Configurações
+            </button>
+            <button className="bg-gray-700 px-4 py-3 rounded-lg">
+              <FaShareAlt size={24} color="#fff" />
+            </button>
+          </div>
+        </section>
+      </Container>
+    </main>
+  );
+}
